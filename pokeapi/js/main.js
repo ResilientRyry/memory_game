b32ed37c941bc2c826e0cb124e663c6014585ada
Ryan T. French <r.tillman.french@gmail.com>
2018-06-27 17:20:20 -0400

    first commit

---
+++ pokeapi/js/main.js
@@ -0,0 +1,9 @@
+//event listener for pokeChoice
+document.getElementById('chik').onclick = chikor()
+// document.getElementById('chim').onclick = chimchar
+// document.getElementById('eev').onclick = eevee
+
+//functions
+function chikor(){
+  document.getElementsByClassName('chikorita').style.display = 'show'
+}
