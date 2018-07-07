08c798062bb086610bce379e4bd24aeb309a0cc8
Ryan Tillman-French <39138118+ResilientRyry@users.noreply.github.com>
2018-07-07 15:06:59 -0400

    Delete main.js

--- pokeapi/js/main.js
+++
@@ -1,9 +0,0 @@
-//event listener for pokeChoice
-document.getElementById('chik').onclick = chikor()
-// document.getElementById('chim').onclick = chimchar
-// document.getElementById('eev').onclick = eevee
-
-//functions
-function chikor(){
-  document.getElementsByClassName('chikorita').style.display = 'show'
-}
